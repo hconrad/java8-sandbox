9f0a24158cfa2b345ce9d564a44a4be527334ff7
Hans Conrad <hans.conrad@gmail.com>
2015-03-15 15:08:31 -0400

    Initial Commit

---
+++ src/main/java/com/conrad/java8-sandbox/App.java
@@ -0,0 +1,13 @@
+package com.conrad.java8-sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
